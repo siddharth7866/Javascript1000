b3bf4c24c5d9b237e6a952f51e53d2f2e02dec76
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-10-02 12:15:40 +0530

    fix

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+console.log("siddharth");
+
+function sid(a) {
+  if (typeof a === "string") {
+    console.log("Please enter number not string");
+  } else {
+    let s = 10 + a;
+    console.log(s);
+  }
+}
+
+sid(10);
+
+//Map function
+
+var s = [1, 2, 3, 4, 5, 6, 7, 8];
+var sid = s.map((x) => x + 100);
+console.log(sid);
+
+function sid() {
+  console.log("siddharth");
+}
+
+setTimeout(sid, 1000);
+console.log("vishen");
+
+console.log(dx);
+dx = 10;
+let dx;
+console.log(dx);
+dx = 10;
+
+var x = 10;
+function sde() {
+  var x = 5;
+  var df = x + 2;
+  console.log(df);
+}
+
+sde();
+console.log(x);
+
+//CLI APP
+var sid = document.getElementsByTagName("h1")[0];
+console.log(sid);
+
+alert("Welcome To Siddharth Cafe");
+function out() {
+  alert("u exit");
+}
+
+sid.addEventListener("mouseover", decide);
+sid.addEventListener("mouseout", out);
+
+function decide() {
+  let userd = prompt("Do you want coffee or read book");
+  if (userd === "coffee") {
+    alert("Please go to the first floor");
+  } else {
+    alert("Please show your library card");
+  }
+}
+
+// decide();
+*/
